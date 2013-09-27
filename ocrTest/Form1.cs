f7962a8fd7a2b7a5d7213012d395931d47b4ab01
Sean Tyler Atwell <sta0003@auburn.edu>
2013-09-26 19:40:29 -0500

    Added the application to the Git
    This shows off how awesome image to text can be.

---
+++ ocrTest/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Aspose.OCR;
+using System.IO;
+
+namespace ocrTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            string filepath = @"C:\Users\Tyler\Desktop\testFile4.png";
+            const string resourceFileName = @"..\..\Resources\Aspose.OCR.Resouces.zip";
+
+            // Initialize OcrEngine
+            OcrEngine ocr = new OcrEngine();
+            // Set the image
+            ocr.Image = ImageStream.FromFile(filepath);
+            // Add language
+            ocr.Languages.AddLanguage(Language.Load("english"));
+            // Load the resource file
+            using (ocr.Resource = new FileStream(resourceFileName, FileMode.Open))
+            {
+                try
+                {
+                    // Process the whole image
+                    if (ocr.Process())
+                    {
+                        // Get the complete recognized text found from the image
+                        label1.Text = ocr.Text.ToString();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    label1.Text = "failure";
+                }
+            }
+
+
+        }
+    }
+}
